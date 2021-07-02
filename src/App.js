54a836b159236fe4f18ec06821316727593a46d1
gfgustavo2015 <gfgustavo2015@gmail.com>
2021-07-02 14:24:16 -0300

    app react para capturar texto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
+import Tesseract from 'tesseract.js';
 import './App.css';
 
 function App() {
+  const [imagePath, setImagePath] = useState("");
+  const [text, setText] = useState("");
+ 
+  const  handleChange =  async (event) => {
+
+    const result = await toBase64(event.target.files[0]).catch(e => Error(e));
+
+    setImagePath(result);
+  }
+  
+  const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+  const handleClick = () => {
+  
+    Tesseract.recognize(
+      imagePath,'eng',
+      { 
+        logger: m => console.log(m) 
+      }
+    )
+    .catch (err => {
+      console.error(err);
+    })
+    .then(result => {
+      // Get Confidence score
+   /*    let confidence = result.confidence */
+      let text = result.data.text;
+     
+      setText(text);
+  
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-main">
+        <h3>Sube una imagen para extraer el texto</h3>
+        <img 
+           src={imagePath} className="App-logo" alt="logo"/>
+        
+          <h3>Extraer texto</h3>
+        <div className="text-box">
+          <p> {text} </p>
+        </div>
+        <input type="file" onChange={handleChange} />
+        <button onClick={handleClick} style={{height:50}}> convert to text</button>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default App
